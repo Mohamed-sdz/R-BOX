77271576cc2ad3d5bdfdf18a80424d056ee661d9
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-06 05:42:16 -0400

    update

---
+++ db.js
@@ -0,0 +1,29 @@
+{
+  "recipes" [
+    {
+      "id": 1,
+      "title": "Recipe 1",
+      "description": "This is the first recipe."
+    },
+    {
+      "id": 2,
+      "title": "Recipe 2",
+      "description": "This is the second recipe."
+    },
+    {
+      "id": 3,
+      "title": "Recipe 3",
+      "description": "This is the third recipe."
+    },
+    {
+      "id": 4,
+      "title": "Recipe 4",
+      "description": "This is the fourth recipe."
+    },
+    {
+      "id": 5,
+      "title": "Recipe 5",
+      "description": "This is the fifth recipe."
+    }
+  ]
+}
